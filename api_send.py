e2fefb9e1cea40ae6528bb69e865515ffd1568f5
Aydin Uzmez <aydin@uzmez.net>
2017-09-09 20:14:49 +0300

    **Update: ** if __main__

--- api_send.py
+++ api_send.py
@@ -65,18 +65,24 @@ class Message(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         return True
 
-try:
-    message1 = Message()
-    saver1 = fusion.Saver()
-    if saver1.is_saver():
-        if saver1.is_there_clip():
-            path = saver1.get_filename()[1]
-            message1.write(path)
-            print "Project's Path:  " + path
-        else:
-            print "#Info: There isn't any project's path in the node"
-    else:
-        print "#Info: This is no The Saver or The Loader"
 
-except urllib2.HTTPError,e:
-    print (e.code,e.msg)
+def run():
+    try:
+        message1 = Message()
+        saver1 = fusion.Saver()
+        if saver1.is_saver():
+            if saver1.is_there_clip():
+                path = saver1.get_filename()[1]
+                message1.write(path)
+                print "Project's Path:  " + path
+            else:
+                print "#Info: There isn't any project's path in the node"
+        else:
+            print "#Info: This is no The Saver or The Loader"
+
+    except urllib2.HTTPError,e:
+        print (e.code,e.msg)
+
+
+if __name__ == '__main__':
+    run()
