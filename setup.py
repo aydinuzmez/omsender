4b0a606925c897b7cb205820a47a46867896596c
Aydin Uzmez <aydinuzmez@hotmail.com>
2018-02-05 04:07:06 +0200

    setup.py was an upgrade to v0.02

--- setup.py
+++ setup.py
@@ -3,12 +3,20 @@ from distutils.core import setup
 setup(
     name='omsender',
     packages= ['omsender'],
-    version='0.01',
+    version='0.02',
     description='Your path send other person with Output Messenger on Blackmagic Fusion.',
     author='Aydin Uzmez',
     author_email='aydinuzmez@gmail.com',
     url='https://github.com/aydinuzmez/omsender',
     download_url= 'https://github.com/aydinuzmez/omsender.git',
     keywords = ['Output Messenger sender','Lan Messenger sender'],
-    classifiers = [],
+    classifiers = [  
+	'Development Status :: 4 - Beta',
+    'Environment :: Plugins',
+	'Operating System :: MacOS',
+	'Operating System :: Microsoft :: Windows',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 2',
+	'Programming Language :: Python :: 3',
+	'Topic :: Communications :: Chat',
 )
